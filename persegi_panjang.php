52fedf18e923b1b06ff33d9230155fc84a2928a1
Dunnn <91164684+Dundolp@users.noreply.github.com>
2022-03-27 14:13:37 +0700

    Add files via upload

---
+++ persegi_panjang.php
@@ -0,0 +1,22 @@
+<?php
+// membuat class persegi panjang
+class PersegiPanjang{
+    public $panjang;
+    public $lebar;
+
+    function __construct($panjang, $lebar){
+        $this->panjang = $panjang;
+        $this->lebar = $lebar;
+    }
+     function luas(){
+        $luas = $this->panjang * $this->lebar;
+        return $luas;
+
+    }
+     function keliling(){
+        $keliling = 2 * ($this->panjang + $this->lebar);
+        return $keliling;
+    }
+
+}
+?>
